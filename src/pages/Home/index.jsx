bf8d48fb28fa89c32769a4aa5c6a3dd5286a206d
usaid AKA <ujedkemal@gmail.com>
2024-05-01 00:52:14 +0700

    fist commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,52 +1,14 @@
-import { useEffect } from 'react';
-import isEmpty from 'lodash/isEmpty';
-import PropTypes from 'prop-types';
-import { connect, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { createStructuredSelector } from 'reselect';
-import { FormattedMessage } from 'react-intl';
-
-import { fetchPokemon } from './actions';
-import { selectPokemon } from './selectors';
-
 import classes from './style.module.scss';
 
-const Home = ({ pokemon }) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    dispatch(fetchPokemon())
-  }, [dispatch]);
+const Home = () => {
 
   return (
     <div className={classes.home}>
-      <div className={classes.homeWrapper}>
-        <h1 className={classes.title}>
-          <FormattedMessage id="app_greeting" />
-        </h1>
-        <div className={classes.lengthWrapper}>
-          <div className={classes.length}>
-            {pokemon.length}++
-          </div>
-        </div>
-        <div className={classes.buyNow} onClick={() => navigate('/courses')}>
-          <FormattedMessage id="navigate_to_product_texts" />
-        </div>
-        {pokemon?.map((item) => {
-          return <Cards />
-        })}
-      </div>
+      <h2>test</h2>
     </div>
   );
 };
 
-Home.propTypes = {
-  pokemon: PropTypes.object,
-};
 
-const mapStateToProps = createStructuredSelector({
-  pokemon: selectPokemon
-});
 
-export default connect(mapStateToProps)(Home);
+export default Home;

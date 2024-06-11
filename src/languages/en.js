1c13c1b62e31978e7d0253175710fb8ce4dde953
usaid AKA <ujedkemal@gmail.com>
2024-06-11 19:43:11 +0700

    basic set up

--- src/languages/en.js
+++ src/languages/en.js
@@ -1,6 +1,6 @@
 export default {
   app_greeting: 'Hi from Web!',
-  app_title_header: 'Bootcamp',
+  app_title_header: 'JAKINDO JAYA LEKASA',
   app_not_found: 'Page not found',
   app_lang_id: 'Indonesian',
   app_lang_en: 'English',
--- src/languages/id.js
+++ src/languages/id.js
@@ -1,6 +1,6 @@
 export default {
   app_greeting: 'Hai dari Web!',
-  app_title_header: 'Bootcamp',
+  app_title_header: 'JAKINDO JAYA LEKASA',
   app_not_found: 'Halaman tidak ditemukan',
   app_lang_id: 'Bahasa Indonesia',
   app_lang_en: 'Bahasa Inggris',
